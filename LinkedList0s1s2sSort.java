a544b093a0917efd73c53b8b7fb6f401dfc969f3
vishalgaur752 <vishalgaur752@gmail.com>
2024-06-03 20:19:47 +0530

    InstaDSA

---
+++ LinkedList0s1s2sSort.java
@@ -0,0 +1,52 @@
+public class LinkedList0s1s2sSort {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empty.");
+            return;
+        }
+        Node temp = head;
+        while (temp != head) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+  
+
+    public static void main(String[] args) {
+
+    }
+}
