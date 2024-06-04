a04a06b3116b02ddad08fb1d957c826895f87c90
vishalgaur752 <vishalgaur752@gmail.com>
2024-06-04 11:18:42 +0530

    InstaDSA

---
+++ DeleteMiddleNode.java
@@ -0,0 +1,76 @@
+public class DeleteMiddleNode {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode = head.next;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empty.");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static Node deletMiddleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+        if (fast.next == null) {
+            return null;
+        }
+        while (fast.next.next != null && fast.next.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(7);
+        head.next.next.next.next = new Node(1);
+        head.next.next.next.next.next = new Node(2);
+        Node temp = deletMiddleNode(head);
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
